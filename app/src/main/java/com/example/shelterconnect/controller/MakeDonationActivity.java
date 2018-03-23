8744b3945aa2a25c6a6577113e5b3c75b42c46b5
Jack Martin <jmartin103@student.gsu.edu>
2018-03-22 22:50:21 -0400

    Attempt at implementing in-app billing. Will likely need help modifying if we must.

--- app/src/main/java/com/example/shelterconnect/controller/MakeDonationActivity.java
+++ app/src/main/java/com/example/shelterconnect/controller/MakeDonationActivity.java
@@ -1,15 +1,70 @@
 package com.example.shelterconnect.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
+import com.anjlab.android.iab.v3.BillingProcessor;
+import com.anjlab.android.iab.v3.TransactionDetails;
 import com.example.shelterconnect.R;
 
-public class MakeDonationActivity extends AppCompatActivity {
+public class MakeDonationActivity extends AppCompatActivity implements BillingProcessor.IBillingHandler {
+    BillingProcessor bp;
+    private Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_donation);
+
+        bp = new BillingProcessor(this, null, this);
+
+        submit = (Button) findViewById(R.id.btnSubmit);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bp.purchase(MakeDonationActivity.this, "donation");
+            }
+        });
+    }
+
+    @Override
+    public void onProductPurchased(@NonNull String productId, @Nullable TransactionDetails details) {
+
+    }
+
+    @Override
+    public void onPurchaseHistoryRestored() {
+
+    }
+
+    @Override
+    public void onBillingError(int errorCode, @Nullable Throwable error) {
+
+    }
+
+    @Override
+    public void onBillingInitialized() {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!bp.handleActivityResult(requestCode, resultCode, data)) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+    
+    @Override
+    public void onDestroy() {
+        if (bp != null) {
+            bp.release();
+        }
+        super.onDestroy();
     }
 }
